ac08446064ef8c5197a67c5b0e82cd5aff41a4ec
Theo Leanse <theoleanse@gmail.com>
2017-02-18 16:31:51 +0000

    WIP: tests to guide the class

--- test/server/app.spec.js
+++ test/server/app.spec.js
@@ -5,5 +5,46 @@ const cheerio = require('cheerio');
 const server = require('../../server/app.js');
 
 describe('CV serving app', () => {
+	it('should serve CV on the \'/\' route', () => {
 
+	})
+
+	it('should call the cyf-github api for extra data', () => {
+
+	})
+
+	it('should render extra data when provided a query param', () => {
+		
+	})
 });
+
+const controller = require('../../server/controllers/cv-controller');
+
+describe('CV controller', () => {
+	it('should do all the above, but in a different place', () => {
+
+	})
+})
+
+const middleware = require('../../server/middleware');
+
+describe('middleware', () => {
+
+	it('should add URL parameters to the response object', () => {
+
+	})
+
+	it('should add query parameters to the response object', () => {
+
+	})
+
+	
+	it('should add headers to the response object', () => {
+
+	})
+
+	
+	it('should add cookies to the response object', () => {
+
+	})
+})
