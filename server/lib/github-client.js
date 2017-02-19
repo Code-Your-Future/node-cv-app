e1597d42e1f7f722184bd7033182b459b6139ad8
Md-Almasri <mohieddin.almasri@hotmail.com>
2017-02-19 21:32:56 +0000

    Api client

--- server/lib/github-client.js
+++ server/lib/github-client.js
@@ -3,6 +3,21 @@ const fetch = require('isomorphic-fetch');
 
 function fetchFromGithub (path) {
 	// write something that will fetch json information from github
+	const userNameDataUrl = host + path;
+	console.log('userNameDataUrl',userNameDataUrl);
+	return (
+		fetch(userNameDataUrl, {
+    	method: 'GET',
+  	})
+  	.then((apiRes) => {
+    	return apiRes.json();
+  	})
+  	.then((json) => {
+    	return json;
+  	})
+  	.catch((err) => {
+    	console.error(err);
+  	}));
 }
 
 module.exports.getUserProfile = function (username) {
@@ -10,3 +25,7 @@ module.exports.getUserProfile = function (username) {
 };
 
 // add a method to retrieve user pull requests here
+module.exports.getUserPullRequests = function (username) {
+	return fetchFromGithub(`/users/${username}/events/public`).then(data => data.filter(value => value.type === 'PullRequestEvent'));
+};
+
