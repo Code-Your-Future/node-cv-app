45a6fa6b59d4ed8cf16165ea912ffc4b98c2db2a
Theo <theoleanse@gmail.com>
2017-02-19 09:44:05 +0000

    add end-to-end tests (#5)

---
+++ test/server/lib/github-client.spec.js
@@ -0,0 +1,31 @@
+const { expect } = require('chai');
+const nock = require('nock');
+
+const githubClient = require('../../../server/lib/github-client.js');
+
+describe('Github client', () => {
+	it('should retrieve user profile information', () => {
+		nock('http://cyf-github-api')
+			.get('/users/wheresrhys')
+			.reply(200, require('../../fixtures/user.json'));
+		return githubClient.getUserProfile('wheresrhys')
+     		.then(user => {
+     			expect(user.login).to.equal('wheresrhys');
+     			expect(user.name).to.equal('Rhys Evans');
+     		})
+	})
+
+	it('should retrieve user pull requests', () => {
+		nock('http://cyf-github-api')
+			.get('/users/wheresrhys/events/public')
+			.reply(200, require('../../fixtures/user.json'));
+		return githubClient.getUserPullRequests('wheresrhys')
+     		.then(prs => {
+     			expect(prs.length).to.equal(4);
+     			prs.forEach(event => {
+     				expect(event.type).to.equal('PullRequestEvent');
+     				expect(event.actor.login).to.equal('wheresrhys')
+     			})
+     		})
+	})
+});
