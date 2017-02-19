88e59b347305d2c0e1d5a5e3d7ca5c2ad5329478
Md-Almasri <mohieddin.almasri@hotmail.com>
2017-02-19 22:25:05 +0000

    indentation issue

--- server/lib/github-client.js
+++ server/lib/github-client.js
@@ -2,30 +2,30 @@ const host = 'http://cyf-github-api.herokuapp.com';
 const fetch = require('isomorphic-fetch');
 
 function fetchFromGithub (path) {
-	// write something that will fetch json information from github
-	const userNameDataUrl = host + path;
-	console.log('userNameDataUrl',userNameDataUrl);
-	return (
-		fetch(userNameDataUrl, {
-    	method: 'GET',
-  	})
-  	.then((apiRes) => {
-    	return apiRes.json();
-  	})
-  	.then((json) => {
-    	return json;
-  	})
-  	.catch((err) => {
-    	console.error(err);
-  	}));
+  // write something that will fetch json information from github
+  const userNameDataUrl = host + path;
+  console.log('userNameDataUrl',userNameDataUrl);
+  return (
+    fetch(userNameDataUrl, {
+      method: 'GET',
+    })
+    .then((apiRes) => {
+      return apiRes.json();
+    })
+    .then((json) => {
+      return json;
+    })
+    .catch((err) => {
+      console.error(err);
+    }));
 }
 
 module.exports.getUserProfile = function (username) {
-	return fetchFromGithub(`/users/${username}`);
+  return fetchFromGithub(`/users/${username}`);
 };
 
 // add a method to retrieve user pull requests here
 module.exports.getUserPullRequests = function (username) {
-	return fetchFromGithub(`/users/${username}/events/public`).then(data => data.filter(value => value.type === 'PullRequestEvent'));
+  return fetchFromGithub(`/users/${username}/events/public`).then(data => data.filter(value => value.type === 'PullRequestEvent'));
 };
 
