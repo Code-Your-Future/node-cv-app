97212249688061706e9b6224e44f2eaf4a8c2e46
ehmed-kino <ehmedaff@gmail.com>
2017-02-20 01:11:43 +0000

    adding users parameters and thier pull request

--- server/app.js
+++ server/app.js
@@ -12,7 +12,8 @@ app.set('view engine', 'hbs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', cvController)
+app.get('/users/:username', cvController);
+app.get('/users/:username/events/public', cvController);
 // add your routes here :)
 
 // app.get('/', (req, res) => {
--- server/controllers/cv-controller.js
+++ server/controllers/cv-controller.js
@@ -1,10 +1,20 @@
 const client = require('../lib/github-client');
-console.log(client.getUserProfile());
+console.log(client.getUserProfile);
 module.exports = (req, res) => {
-  return client.getUserProfile()
+  return client.getUserProfile(req.params.username)
   .then((data) =>{
     res.send(data);
   }).catch(err => {
-    console.log('Not retriving Data');
+    console.log(err);
   })
-};
+}
+
+module.exports = (req, res) => {
+  return client.getUserPullRequests(req.params.username)
+  .then((data) => {
+    res.send(data);
+  })
+  .catch(err => {
+    console.log(err);
+  })
+}
