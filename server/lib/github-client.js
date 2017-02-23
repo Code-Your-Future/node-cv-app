20f40975cf796fc8056cae44c3b4a62678cf0eb2
Md-Almasri <mohieddin.almasri@hotmail.com>
2017-02-23 15:50:21 +0000

    using ES6

--- server/lib/github-client.js
+++ server/lib/github-client.js
@@ -1,31 +1,20 @@
 const host = 'http://cyf-github-api.herokuapp.com';
 const fetch = require('isomorphic-fetch');
 
-function fetchFromGithub (path) {
-  // write something that will fetch json information from github
-  const userNameDataUrl = host + path;
-  console.log('userNameDataUrl',userNameDataUrl);
+fetchFromGithub = path => {
   return (
-    fetch(userNameDataUrl, {
-      method: 'GET',
-    })
-    .then((apiRes) => {
-      return apiRes.json();
-    })
-    .then((json) => {
-      return json;
-    })
-    .catch((err) => {
-      console.error(err);
-    }));
+    fetch(`${host}${path}`)
+    .then(apiRes => apiRes.json())
+    .then(json => json)
+    .catch(err => console.error(err))
+    );
 }
 
-module.exports.getUserProfile = function (username) {
-  return fetchFromGithub(`/users/${username}`);
-};
+module.exports.getUserProfile = username => fetchFromGithub(`/users/${username}`);
 
-// add a method to retrieve user pull requests here
-module.exports.getUserPullRequests = function (username) {
-  return fetchFromGithub(`/users/${username}/events/public`).then(data => data.filter(value => value.type === 'PullRequestEvent'));
+module.exports.getUserPullRequests = username => {
+  return (
+    fetchFromGithub(`/users/${username}/events/public`)
+    .then(data => data.filter(value => value.type === 'PullRequestEvent'))
+    );
 };
-
